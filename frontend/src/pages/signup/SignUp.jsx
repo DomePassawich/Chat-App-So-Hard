fc5b66eda8e93c267c4086eecee955a799d74522
DomePassawich <passawich.klinhomhual@gmail.com>
2025-04-20 20:46:24 +0700

    Update SignUp.jsx

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { use, useState } from 'react'
-import GenderCheckBox from './GenderCheckBox.jsx';
+import GenderCheckBox from './GenderCheckbox.jsx';
 import { Link } from 'react-router-dom';
 import useSignup from '../../้hooks/useSignup';
 
