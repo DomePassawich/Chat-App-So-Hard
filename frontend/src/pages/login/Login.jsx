8f4d0664724b63c26a13d1a1e557f4b6fdc3503e
DomePassawich <passawich.klinhomhual@gmail.com>
2025-04-20 19:40:29 +0700

    4th part complteted with socket

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,36 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import useLogin from '../../้hooks/useLogin';
 
 const Login = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+const {loading,login} = useLogin()
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username,password)
+  }
+
+
   return <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
     <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg' style={{ backgroundColor: 'rgba(255, 255, 255, 0)' }}>
       <h1 className='text-3xl font-semibold text-center text-gray-700'>
         Login
         <span className='text-blue-950'> Chat app</span>
       </h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label className='label p-2.5'>
             <span className='text-base label-text'>Username</span>
           </label>
-          <input type='text' placeholder='Enter Username' className='w-full input input-bordered h-10' />
+          <input type='text' placeholder='Enter Username' className='w-full input input-bordered h-10' 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div>
           <label className='label p-2.5'>
             <span className='text-base label-text'>Password</span>
           </label>
-          <input type='text' placeholder='Enter Password' className='w-full input input-bordered h-10' />
+          <input type='password' placeholder='Enter Password' className='w-full input input-bordered h-10' 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block p-1'>
+        <Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block p-1'>
           Don't have an account?
-        </a>
+        </Link>
+
         <div>
-          <button className='btn btn-block btn-sm mt-2'>Login</button>
+          <button className='btn btn-block btn-sm mt-2' disabled={loading}>
+            {loading ? <span className='loading loading-spinner'></span> : "Login"}
+          </button>
           
         </div>
       </form>
     </div>
-
-
   </div>
 };
 
--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
-const GenderCheckBox = () => {
+const GenderCheckBox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className='flex p-2 mt-2'>
       <div className='form-control'>
-        <label className={`label gap-2 cursor-pointer`}>
+        <label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""}`}>
           <span className='label-text'>Male</span>
-          <input type="checkbox" defaultChecked className="checkbox checkbox-info" />
+          <input type="checkbox" className="checkbox checkbox-info"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
         </label>
       </div>
       <div className='flex ml-1'>
-        <label className={`label gap-2 cursor-pointer`}>
+        <label className={`label gap-2 cursor-pointer ${selectedGender === "female" ? "selected" : ""}`}>
           <span className='label-text'>Female</span>
-          <input type="checkbox" defaultChecked className="checkbox checkbox-secondary" />
+          <input type="checkbox" className="checkbox checkbox-secondary"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
         </label>
       </div>
     </div>
